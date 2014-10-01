549f14ccfa0a125fc162bf839ae011c90c1f2e82
JayChetty <jaypaulchetty@gmail.com>
2014-10-01 14:05:03 +0100

    describe drawable object and what an object extending it should have

--- client/drawable.js
+++ client/drawable.js
@@ -1,5 +1,9 @@
 var drawable = {
-
+  // mixing this in allow to be rendered by canvas view
+  // objects should describe a position, 
+  // an image for which to be drawn TODO
+  // also possibly a pattern so they can eg walk TODO
+  
   updateView: function(){
     if(this.view){
       this.view.render();
